1d2d86dbc0e840fb87081e2113141e0d19e79df1
MIXO11 <jmxd111195@gmail.com>
2023-06-01 23:15:37 +0800

    portfolio done

---
+++ src/components/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export const About = () => {
+  return (
+    <div name='about'
+        className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+        <div className='max-w-screen-lg p-4 mx-auto flex-col justify-center w-full h-full'>
+            <div className='pb-8 mt-40'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+            </div>
+            <p className='text-xl mt-20'>
+                I am a technical support professional. I've always been interested in technology and computers, which led me to pursue a degree in BSIT. During my free time, I discovered a passion for coding and software development. When not working, I enjoy playing mobile games and exploring in programming. I hope to continue to grow and develop my skills as a software engineer and eventually become a successful programmer.
+            </p>
+
+            <br />
+
+            <p className='text-xl'>
+                I am a technical support professional. I've always been interested in technology and computers, which led me to pursue a degree in BSIT. During my free time, I discovered a passion for coding and software development. When not working, I enjoy playing mobile games and exploring in programming. I hope to continue to grow and develop my skills as a software engineer and eventually become a successful programmer.
+            </p>
+        </div>
+    </div>
+  );
+};
